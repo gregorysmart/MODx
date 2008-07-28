2d7efdbbcc396391b050964eab6559f1cfedb4e8
opengeek <opengeek@0a542878-baf4-0310-9890-ffb925a0de40>
2008-07-28 20:37:30 +0000

    * [#MODX-174] Language setting in setup is not loaded. 	- dirname(dirname($_SERVER['REQUEST_URI'])) . '/' seems to be a better global solution than '/' or regex.

--- setup/processors/language.php
+++ setup/processors/language.php
@@ -3,5 +3,5 @@ $language= 'en';
 if (isset ($_REQUEST['language'])) {
     $language= $_REQUEST['language'];
 }
-setcookie('modx_setup_language', $language, 0, '/');
+setcookie('modx_setup_language', $language, 0, dirname(dirname($_SERVER['REQUEST_URI'])) . '/');
 $this->error->success('welcome');
