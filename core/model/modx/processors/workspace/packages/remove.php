e32479b3b827a88dda4fff3b932ace682401a30b
splittingred <splittingred@0a542878-baf4-0310-9890-ffb925a0de40>
2008-10-02 21:25:23 +0000

    - Making sure removing the package actually removes the transport package file.

--- core/model/modx/processors/workspace/packages/remove.php
+++ core/model/modx/processors/workspace/packages/remove.php
@@ -13,7 +13,15 @@ $modx->log(XPDO_LOG_LEVEL_INFO,'Grabbing package to remove...');
 $package = $modx->getObject('transport.modTransportPackage', $_REQUEST['signature']);
 if ($package == null) $modx->error->failure($modx->lexicon('package_err_nfs',array('signature' => $_REQUEST['signature'])));
 
-$modx->log(XPDO_LOG_LEVEL_INFO,'Successfully grabbed package. Now attempting to remove...');
+$modx->log(XPDO_LOG_LEVEL_INFO,'Successfully grabbed package. Now attempting to remove actual file...');
+
+$f = $modx->config['core_path'].'packages/'.$package->signature.'.transport.zip';
+if (!file_exists($f)) {
+    $modx->log(XPDO_LOG_LEVEL_ERROR,'Transport file was not found and could not be removed from the core/packages directory.');
+} else if (!@unlink($f)) {
+    $modx->log(XPDO_LOG_LEVEL_ERROR,'Transport file was unable to be removed, check your permissions.');
+}
+
 if ($package->remove() == false) {
     $modx->log(XPDO_LOG_LEVEL_ERROR,$modx->lexicon('package_err_remove'));
     $modx->error->failure($modx->lexicon('package_err_remove',array('signature' => $package->getPrimaryKey())));
