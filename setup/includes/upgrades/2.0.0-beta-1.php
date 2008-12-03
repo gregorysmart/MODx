e482f12b92573b0174837b726d67dc1c2c1ab6fd
splittingred <splittingred@0a542878-baf4-0310-9890-ffb925a0de40>
2008-12-03 15:12:53 +0000

    - Added beta-1 upgrade script for dealing with renegade menu actions that are deprecated

---
+++ setup/includes/upgrades/2.0.0-beta-1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Specific upgrades for Revolution 2.0.0-beta-1
+ *
+ * @package setup
+ */
+
+/* remove some menu items that are deprecated from alpha */
+/* first, remove the dashes from revo-alpha */
+$dashes = $this->install->xpdo->getCollection('modMenu',array( 'text' => '-'));
+foreach ($dashes as $dash) {
+    $dash->remove();
+}
+
+/* remove link to home */
+$home = $this->install->xpdo->getObject('modMenu',array('text' => 'home'));
+if ($home != null) $home->remove();
+
+/* remove logout link */
+$logout = $this->install->xpdo->getObject('modMenu',array('text' => 'logout'));
+if ($logout != null) $logout->remove();
