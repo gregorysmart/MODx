f7e424c6cc9fd68fef6a19466389dd3388c97bf6
splittingred <splittingred@0a542878-baf4-0310-9890-ffb925a0de40>
2009-03-03 16:40:45 +0000

    - Fix to date TV default value

--- core/model/modx/processors/element/tv/renders/mgr/input/date.php
+++ core/model/modx/processors/element/tv/renders/mgr/input/date.php
@@ -6,7 +6,9 @@
 $this->xpdo->lexicon->load('tv_widget');
 
 $v = $this->get('value');
-$v = strftime('%Y-%m-%d',strtotime($v));
+if ($v != '' && $v != '0' && $v != '0000-00-00 00:00:00') {
+    $v = strftime('%Y-%m-%d',strtotime($v));
+}
 $this->set('value',$v);
 
 return $this->xpdo->smarty->fetch('element/tv/renders/input/date.tpl');
--- manager/templates/default/element/tv/renders/input/date.tpl
+++ manager/templates/default/element/tv/renders/input/date.tpl
@@ -1,8 +1,6 @@
 <input id="tv{$tv->id}" name="tv{$tv->id}"
 	type="text" class="datefield"
 	value="{$tv->get('value')}"
-	modx:format="Y-m-d H:i:s"
-	modx:allowblank="1"
 	onblur="javascript:triggerDirtyField(this);"
 />
 
