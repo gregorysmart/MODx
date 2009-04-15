823f1bc40ca236657514964422a22f1c4c034f05
opengeek <opengeek@0a542878-baf4-0310-9890-ffb925a0de40>
2009-04-15 04:13:18 +0000

    * modDbRegister: allow messages to be updated/overwritten

--- core/model/modx/registry/moddbregister.class.php
+++ core/model/modx/registry/moddbregister.class.php
@@ -232,10 +232,12 @@ class modDbRegister extends modRegister {
                                 $payload = '';
                                 if ($expires > 0) $payload.= "if (time() > {$expires}) return null;\n";
                                 $payload.= 'return ' . var_export($msg, true) . ";\n";
-                                $messageObj = $this->modx->newObject('registry.db.modDbRegisterMessage');
-                                if ($messageObj) {
+                                if (!$messageObj = $this->modx->getObject('registry.db.modDbRegisterMessage', array('topic' => $topicObj->get('id'), 'id' => 'msgKey'))) {
+                                    $messageObj = $this->modx->newObject('registry.db.modDbRegisterMessage');
                                     $messageObj->set('topic', $topicObj->get('id'));
                                     $messageObj->set('id', $msgKey);
+                                }
+                                if ($messageObj) {
                                     $messageObj->set('created', strftime('%Y-%m-%d %H:%M:%S'));
                                     $messageObj->set('valid', strftime('%Y-%m-%d %H:%M:%S', $timestamp));
                                     $messageObj->set('expires', $expires);
